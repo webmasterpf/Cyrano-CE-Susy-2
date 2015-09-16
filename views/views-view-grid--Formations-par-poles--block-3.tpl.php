196d01698206a0f5cd4ec95ec7525e908751c2e7
webmasterpf-DG <webmaster@provence-formation.com>
2015-09-16 16:38:13 +0200

    Dev HP celony avec flexbox

---
+++ views/views-view-grid--Formations-par-poles--block-3.tpl.php
@@ -0,0 +1,20 @@
+<?php
+// $Id: views-view-grid.tpl.php,v 1.3.4.1 2010/03/12 01:05:46 merlinofchaos Exp $
+/**
+ * @file views-view-grid.tpl.php
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ *
+ * @ingroup views_templates
+ *
+ * Template pour la vue-bloc Pole Services sur page Pôles formation
+ *
+ *Bloc pole arts appliqués
+ */
+?>
+<?php
+global $theme_path;
+include ($theme_path.'/includes/inc_poles_table_formation.php');
+?>
