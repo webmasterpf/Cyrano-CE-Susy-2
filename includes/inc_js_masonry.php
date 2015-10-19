d2c70a3308f344ca9916e1f1ce15ba243857f918
webmasterpf-DG <webmaster@provence-formation.com>
2015-10-19 16:14:45 +0200

    change classe pour breadcrumb + retouche sur masonry sans succès

--- includes/inc_js_masonry.php
+++ includes/inc_js_masonry.php
@@ -5,7 +5,7 @@ jQuery111(document).ready(function($) {
   $masonryContainer.masonry({
     columnWidth: '.masonry__column',
     gutter: '.masonry__gutter',
-    itemSelector: '.masonry__item'
+    itemSelector: '.masonry__item,.masonry--item'
   });
 });
 </script>
